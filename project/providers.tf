b278d1523d73f389309a4e2c6029c9c6a6ebd4d3
zabronax <zabronax@gmail.com>
2026-02-03 17:42:23 +0100

    setup iac backend configuration

--- project/providers.tf
+++ project/providers.tf
@@ -5,6 +5,15 @@ terraform {
       version = "6.10.2"
     }
   }
+
+  # This is Hetzner's Object Storage (Ceph) for Terraform state.
+  backend "s3" {
+    bucket = "zab-projects"
+    key    = "reference-documentation/project/terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  } 
 }
 
 variable "github_token" {
